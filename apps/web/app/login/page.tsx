350cd42327f1d17f30ac4a60b8e7103f12cd31f0
TECHINNNNNNNN <chompooborisuthtechin@gmail.com>
2026-03-30 21:32:44 +0700

    feat: replace Link component with next-view-transitions for improved navigation transitions on login page

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+import { Link } from "next-view-transitions";
 import { signIn, useSession } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
