9b8785d34ee7c587229fd5152283f3616eb2bb93
TECHINNNNNNNN <chompooborisuthtechin@gmail.com>
2026-04-19 12:32:45 +0700

    refactor: remove ReactQueryDevtools from Providers component to streamline code and improve performance

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function makeQueryClient() {
   return new QueryClient({
@@ -37,7 +36,6 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
